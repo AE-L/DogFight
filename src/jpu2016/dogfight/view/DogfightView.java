f049dfd594b2c67f42fe00507d98782f8bbbf092
Nenrikido <ampy00@live.fr>
2017-06-01 16:50:40 +0200

    Created some things

--- src/jpu2016/dogfight/view/DogfightView.java
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -1,5 +1,30 @@
+/**
+ * 
+ */
 package jpu2016.dogfight.view;
 
-public class DogfightView {
+/**
+ * @author ampy0
+ *
+ */
+public class DogfightView implements IViewSystem {
+
+	/* (non-Javadoc)
+	 * @see jpu2016.dogfight.view.IViewSystem#displayMessage(java.lang.String)
+	 */
+	@Override
+	public String displayMessage(String message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see jpu2016.dogfight.view.IViewSystem#closeAll()
+	 */
+	@Override
+	public void closeAll() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
---
+++ src/jpu2016/dogfight/view/IViewSystem.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package jpu2016.dogfight.view;
+
+/**
+ * @author ampy0
+ *
+ */
+public interface IViewSystem {
+
+}
