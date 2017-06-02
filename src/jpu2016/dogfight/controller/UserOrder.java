eabec9a521655f0faf31258c8af3987416b72555
BornToWard <julien.his@viacesi.fr>
2017-06-02 08:28:26 +0000

    few more classes & interfaces

---
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,15 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder {
+	private int player;
+	
+	public UserOrder(int player, Order order){
+		
+	}
+	public Order getOrder(){
+		
+	}
+	public int getPlayer(){
+		
+	}
+}
