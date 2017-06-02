9be6a4fa7e02d1c806ea4916db111bdea4bd83b1
Nenrikido <ampy00@live.fr>
2017-06-02 10:22:07 +0200

    commit of some other classes/interfaces

--- src/jpu2016/dogfight/view/DogfightView.java
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -2,16 +2,21 @@
  * 
  */
 package jpu2016.dogfight.view;
+import java.lang.Runnable;
 
 /**
  * @author Nenri
  *
  */
-public class DogfightView implements IViewSystem {
+public class DogfightView implements IViewSystem,Runnable {
 
 	/* (non-Javadoc)
 	 * @see jpu2016.dogfight.view.IViewSystem#displayMessage(java.lang.String)
 	 */
+	public DogfightView(IOrderPerformer orderPerformer,IDogfightModel dogfightModel,Observable observable) {
+		
+	}
+	@
 	@Override
 	public String displayMessage(String message) {
 		// TODO Auto-generated method stub
@@ -26,5 +31,9 @@ public class DogfightView implements IViewSystem {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	@Override
+	public void run() {
+		
+	}
 }
---
+++ src/jpu2016/dogfight/view/EventPerformer.java
@@ -0,0 +1,11 @@
+package jpu2016.dogfight.view;
+
+import jpu2016.gameframe.IEventPerformer;
+import jpu2016.gameframe.keyEvent;
+
+public class EventPerformer implements IEventPerformer {
+
+	public void eventPerform(keyEvent keyCode) {
+		
+	}
+}
